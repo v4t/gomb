a1485e048a11d12c8a4296145e97037da4000a2a
v4t <39200058+v4t@users.noreply.github.com>
2020-09-13 21:52:10 +0300

    Add display

--- pkg/graphics/display.go
+++ pkg/graphics/display.go
@@ -1,11 +1,137 @@
 package graphics
 
+import (
+	"image/color"
+	"log"
+	"math"
+
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
+)
+
+// DefaultPalette represents the available colors.
+var DefaultPalette = [4]color.RGBA{
+	color.RGBA{0xe0, 0xf0, 0xe7, 0xff}, // White
+	color.RGBA{0x8b, 0xa3, 0x94, 0xff}, // Light gray
+	color.RGBA{0x55, 0x64, 0x5a, 0xff}, // Dark gray
+	color.RGBA{0x34, 0x3d, 0x37, 0xff}, // Black
+}
+
+// Display screen dimensions
+const (
+	ScreenWidth  = 160
+	ScreenHeight = 144
+)
+
 // Display represents gameboy display.
 type Display struct {
+	window      *pixelgl.Window
+	image       *pixel.PictureData
+	enabled     bool
+	Palette     [4]color.RGBA
+	pixelBuffer [ScreenWidth][ScreenHeight][4]uint8
+	offset      int
+}
 
+// Init initializes display.
+func (display *Display) Init() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "gomb",
+		Bounds: pixel.R(0, 0, ScreenWidth, ScreenHeight),
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	display.Palette = DefaultPalette
+
+	win.Clear(colornames.Black)
+	display.window = win
+	display.image = &pixel.PictureData{
+		Pix:    make([]color.RGBA, ScreenWidth*ScreenHeight),
+		Stride: ScreenWidth,
+		Rect:   pixel.R(0, 0, ScreenWidth, ScreenHeight),
+	}
+
+	pos := win.GetPos()
+	win.SetPos(pixel.ZV)
+	win.SetPos(pos)
+	display.updateCamera()
+	win.Update()
+}
+
+// Init initializes new display.
+func Init() *Display {
+	display := &Display{}
+	display.enabled = true
+	display.Palette = DefaultPalette
+	return display
 }
 
 // IsEnabled boolean flag.
 func (display *Display) IsEnabled() bool {
-	return true
+	return display.enabled
+}
+
+// Enable display.
+func (display *Display) Enable() {
+	display.enabled = true
+}
+
+// Disable display.
+func (display *Display) Disable() {
+	display.enabled = false
+}
+
+// Draw adds pixel to display buffer.
+func (display *Display) Draw(x byte, y byte, colorIndex byte) {
+	if (x < 0 || x >= ScreenWidth) || (y < 0 || y >= ScreenHeight) {
+		log.Println("Invalid x or y:", x, y)
+	}
+	if display.enabled {
+		color := display.Palette[colorIndex]
+		display.pixelBuffer[x][y][0] = color.R
+		display.pixelBuffer[x][y][1] = color.G
+		display.pixelBuffer[x][y][2] = color.B
+		display.pixelBuffer[x][y][3] = color.A
+	}
+}
+
+// RenderImage is called when image frame is complete.
+func (display *Display) RenderImage() {
+	// display.image.Pix = display.imgBuffer
+	img := make([]color.RGBA, ScreenWidth*ScreenHeight)
+	for x := 0; x < ScreenWidth; x++ {
+		for y := 0; y < ScreenHeight; y++ {
+			index := ScreenWidth * x + y
+			img[index] = color.RGBA{
+				display.pixelBuffer[x][y][0],
+				display.pixelBuffer[x][y][1],
+				display.pixelBuffer[x][y][2],
+				display.pixelBuffer[x][y][3]}
+		}
+	}
+
+	display.image.Pix = img
+	bg := color.RGBA{R: 202, G: 220, B: 159, A: 0xff}
+	display.window.Clear(bg)
+
+	spr := pixel.NewSprite(pixel.Picture(display.image), pixel.R(0, 0, ScreenWidth, ScreenHeight))
+	spr.Draw(display.window, pixel.IM)
+
+	display.updateCamera()
+	display.window.Update()
+	display.offset = 0
+}
+
+func (display *Display) updateCamera() {
+	xScale := display.window.Bounds().W() / ScreenWidth
+	yScale := display.window.Bounds().H() / ScreenHeight
+	scale := math.Min(yScale, xScale)
+
+	shift := display.window.Bounds().Size().Scaled(0.5).Sub(pixel.ZV)
+	cam := pixel.IM.Scaled(pixel.ZV, scale).Moved(shift)
+	display.window.SetMatrix(cam)
 }
